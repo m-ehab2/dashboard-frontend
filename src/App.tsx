088d4ef4b1aff9c178983b79b1d118607954d649
m-ehab2 <muhamedehab1@gmail.com>
2024-03-20 18:45:00 +0200

    modify : 'adding announcements and Quizzes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import withAuth from "./components/requireAuth";
 import Status from "./components/dashboard/main/Status";
 import Sidebar from "./components/dashboard/sidebar/Sidebar";
 import Hero from "./components/dashboard/main/Hero";
+import Quizzes from "./components/dashboard/main/quizzes/Quizzes";
+import Announcements from "./components/dashboard/main/announcments/Announcements";
 
 function App() {
   return (
@@ -13,6 +15,14 @@ function App() {
       <Grid sx={{ backgroundColor: "#F7F7F7" }} item md={10}>
         <Status />
         <Hero />
+        <Grid container sx={{ padding: "10px 20px" }}>
+          <Grid sx={{ paddingRight: "20px" }} item md={9}>
+            <Announcements />
+          </Grid>
+          <Grid item md={3}>
+            <Quizzes />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
